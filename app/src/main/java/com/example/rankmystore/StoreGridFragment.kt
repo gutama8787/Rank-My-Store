8f32b8002c093a2f7d507b4fcd4f5a3acc490c57
Edson <edcortes@umd.edu>
2022-04-29 21:10:52 -0400

    finished homescreen design

--- app/src/main/java/com/example/rankmystore/StoreGridFragment.kt
+++ app/src/main/java/com/example/rankmystore/StoreGridFragment.kt
@@ -28,10 +28,22 @@ class StoreGridFragment: Fragment(){
         recyclerView.layoutManager = GridLayoutManager(context, 1, RecyclerView.VERTICAL, false)
 
         val storeList = mutableListOf<StoreEntry>()
-        var product : StoreEntry = StoreEntry()
-        product.storeName = "title"
-        product.storeRating = "rating"
-        storeList.add(product)
+        var product1 : StoreEntry = StoreEntry()
+        product1.storeName = "title1"
+        product1.storeRating = "rating"
+        var product2 : StoreEntry = StoreEntry()
+        product2.storeName = "title 2"
+        product2.storeRating = "rating 2"
+        var product3 : StoreEntry = StoreEntry()
+        product3.storeName = "title3"
+        product3.storeRating = "rating"
+        var product4 : StoreEntry = StoreEntry()
+        product4.storeName = "BRUH"
+        product4.storeRating = "rating 2"
+        storeList.add(product1)
+        storeList.add(product2)
+        storeList.add(product3)
+        storeList.add(product4)
 
         val adapter = panel_adapter(storeList)
         recyclerView.adapter = adapter
